d116c0debbd66c7bed5e032c8502b994294cba43
aimof <aimof.aimof@gmail.com>
2021-07-23 12:07:47 +0900

    Sort articles in Category Post List

--- examples/starter-blog/src/components/PostsList.js
+++ examples/starter-blog/src/components/PostsList.js
@@ -2,6 +2,11 @@ import React from "react"
 import PostsListCard from "./PostsListCard"
 
 const PostsList = ({ postEdges }) => {
+  postEdges.sort((a, b) => {
+    const dateA = new Date(a.node.frontmatter.date)
+    const dateB = new Date(b.node.frontmatter.date)
+    return dateB - dateA
+  })
   return postEdges.map(({ node }) => {
     return <PostsListCard key={node.fields.slug} {...node} />
   })
